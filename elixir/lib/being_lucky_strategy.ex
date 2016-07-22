4271386f3b04e30b5a3d4ae1fe57e3a61a0ee08a
Daniel Z <daniel@softwarecriollo.com>
2016-07-22 11:41:01 +1000

    fixing assets

--- elixir/lib/being_lucky_strategy.ex
+++ elixir/lib/being_lucky_strategy.ex
@@ -1,6 +1,5 @@
 defmodule BeingLuckyStrategy do
   def score(values) do
-    values = [1,1,1,4,5]
     calculation = %{
       1 => fn(x) -> div(x, 3) * 1000 + rem(x, 3) * 100 end,
       5 => fn(x) -> div(x, 3) * 500 + rem(x, 3) * 50 end,
@@ -9,7 +8,6 @@ defmodule BeingLuckyStrategy do
       4 => fn(x) -> div(x, 3) * 400 end,
       6 => fn(x) -> div(x, 3) * 600 end
     }
-
     Enum.group_by(values, fn(x) -> x end)
     |> Enum.map(fn {k, list} -> {k, Enum.count(list)} end)
     |> Enum.reduce(0, fn ({item,quantity},acc) -> (Map.fetch!(calculation,item).(quantity) + acc) end)
--- elixir/test/being_lucky_test.exs
+++ elixir/test/being_lucky_test.exs
@@ -3,14 +3,14 @@ defmodule BeingLuckyStrategyTest do
   doctest BeingLuckyStrategy
 
   test "[1,1,1,1,1]" do
-    BeingLuckyStrategy.score([1, 1, 1, 1, 1]) == 1200
+    assert BeingLuckyStrategy.score([1, 1, 1, 1, 1]) == 1200
   end
 
   test "[5,1,3,4,1]" do
-    BeingLuckyStrategy.score([5, 1, 3, 4, 1]) == 250
+    assert BeingLuckyStrategy.score([5, 1, 3, 4, 1]) == 250
   end
 
   test "[1,1,1,3,1]" do
-    BeingLuckyStrategy.score([1, 1, 1, 3, 1]) == 1100
+    assert BeingLuckyStrategy.score([1, 1, 1, 3, 1]) == 1100
   end
 end
