686b23a7535bd6e5028e8532e971a03f5cd81637
Daniel Zambrano <cacique.coder@gmail.com>
2021-02-14 18:07:07 +1000

    removing fancy code

--- ruby/being_lucky_strategy.rb
+++ ruby/being_lucky_strategy.rb
@@ -1,8 +1,25 @@
 require 'minitest/autorun'
 
 class BeingLuckyStrategy
+  def initialize(results)
+    @results = results
+  end
 
-  # Three 1's => 1000 points
+  def score
+    dice_count_map.map{|dice_value, counting| get_score_by(dice_value, counting) }.sum(0)
+  end
+
+  private
+
+  attr_reader :results
+
+  def dice_count_map
+    @dice_count_map ||=
+      results.group_by(&:to_i)
+             .each_with_object({}) { |(dice, dice_result), hsh| hsh[dice] = dice_result.count }
+  end
+
+    # Three 1's => 1000 points
   # Three 6's =>  600 points
   # Three 5's =>  500 points
   # Three 4's =>  400 points
@@ -11,66 +28,66 @@ class BeingLuckyStrategy
   # One   1   =>  100 points
   # One   5   =>   50 points
 
-  SCORES = {
-    "1" => -> (x) { (x / 3) * 1000 + (x % 3)*100 },
-    "5" => -> (x) { (x / 3) * 500 + (x % 3)*50 },
-    "2" => -> (x) { (x / 3) * 200 },
-    "3" => -> (x) { (x / 3) * 300 },
-    "4" => -> (x) { (x / 3) * 400 },
-    "6" => -> (x) { (x / 3) * 600 }
-  }
-
-  def initialize(results)
-    @pre_result = results.group_by(&:to_i)
-                     .each_with_object({}) { |(k,v), hsh| hsh[k] = v.count }
+  def get_score_by(dice, count)
+    case dice.to_s
+    when "1"
+      (count / 3) * 1000 + (count % 3)*100
+    when "5"
+      (count / 3) * 500 + (count % 3)*50
+    when "2"
+      (count / 3) * 200
+    when "3"
+      (count / 3) * 300
+    when "4"
+      (count / 3) * 400
+    when "6"
+      (count / 3) * 600
+    else
+      0
+    end
   end
-
-  def score
-    @pre_result.map{|k,v| SCORES[k.to_s].call(v) }.reduce(&:+)
-  end
-
 end
 
 describe BeingLuckyStrategy do
   it "[5,1,3,4,1]" do
     score = BeingLuckyStrategy.new([5,1,3,4,1]).score
-    score.must_equal 250
+    _(score).must_equal 250
   end
   it "[1,1,1,3,1]" do
     score = BeingLuckyStrategy.new([1,1,1,3,1]).score
-    score.must_equal 1100
+    _(score).must_equal 1100
   end
   it "[2,4,4,5,4]" do
     score = BeingLuckyStrategy.new([2,4,4,5,4]).score
-    score.must_equal 450
+    _(score).must_equal 450
   end
   it " [1,1,1,1,1,1]" do
     score = BeingLuckyStrategy.new([1,1,1,1,1,1]).score
-    score.must_equal 2000
+    _(score).must_equal 2000
   end
   it " [2,2,2,2,2,2]" do
     score = BeingLuckyStrategy.new([2,2,2,2,2,2]).score
-    score.must_equal 400
+    _(score).must_equal 400
   end
 
   it " [1,1,1,2,2,2]" do
     score = BeingLuckyStrategy.new([1,1,1,2,2,2]).score
-    score.must_equal 1200
+    _(score).must_equal 1200
   end
 
   it " [1,2,3,5,1,1]" do
     score = BeingLuckyStrategy.new([1,2,3,5,1,1]).score
-    score.must_equal 1050
+    _(score).must_equal 1050
   end
 
   it " [1,5,5,5,1,1]" do
     score = BeingLuckyStrategy.new([1,5,5,5,1,1]).score
-    score.must_equal 1500
+    _(score).must_equal 1500
   end
 
   it "[5,5,5,5,5,5]" do
     score = BeingLuckyStrategy.new([5,5,5,5,5,5]).score
-    score.must_equal 1000
+    _(score).must_equal 1000
   end
 
 end
